bb7a3c6581aa89248e67375abf7812bd97e8c13a
unknown <mandalsalman28818@gmail.com>
2023-12-04 20:40:13 +0530

    get router add

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 
 //Route Exports
-const user=require("./routes/userroutes");
+const user = require("./routes/userroutes");
 
 
 app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Route working"
+    })
+})
 
 
 
 //Routes
-app.use("/api/v1",user);
+app.use("/api/v1", user);
 
 
 
