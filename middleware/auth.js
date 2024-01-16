3cb4b73b3cd8a12331c5623d6699154079ce12a7
unknown <mandalsalman28818@gmail.com>
2024-01-16 17:05:40 +0530

    auth modified

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,24 @@
-const jwt = require('jsonwebtoken');
-const catchAsyncError = require('./path-to-your-async-error-handler'); // Import your async error handler
-const User = require('./path-to-your-user-model'); // Import your User model
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncError = require("./catchAsyncError");
+const User = require("../models/userModel");
 
-exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-  const token = req.headers.authorization;
 
-  if (!token) {
-    return next(new ErrorHandler('Please provide a token', 401));
-  }
+exports.isAuthenticatedUser = catchAsyncError(
+    async (req, res, next) => {
+        const token = req.headers.authorization;
 
-  try {
-    const decodedUserData = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decodedUserData.id);
-    next();
-  } catch (error) {
-    return next(new ErrorHandler('Invalid token', 401));
-  }
-});
+        if (!token) {
+            return next(new ErrorHandler('Please provide a token', 401));
+        }
+
+
+        try {
+            const decodedUserData = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decodedUserData.id);
+            next();
+        } catch (error) {
+            return next(new ErrorHandler('Invalid token', 401));
+        }
+    }
+)
